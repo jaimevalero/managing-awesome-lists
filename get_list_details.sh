617db9090e1060d0a6f1894eb12a2d1636b6cdc5
jaimevalero <jaimevalero78@yahoo.es>
2019-08-15 17:53:36 +0200

    Awesome listin: This account is pure gold https://t.co/k6dhsSw5NY

--- get_list_details.sh
+++ get_list_details.sh
@@ -10,29 +10,36 @@
 source ./.credentials
 #CREDENTIALS="replace-for-your-github-user:replace-for-your-github-password"
 
+Generate_Single_List( )
+{
+
+  AWESOME_LIST_URL=$1
+  URI=`echo "${AWESOME_LIST_URL}" | egrep -o -e 'github.com/.*' | cut -d\/ -f2-3`
+  OUTPUT_FILE=`echo $URI | tr \/ @  | sed -e 's@$@.md@'`
+
+  echo '| Link  | Stars   | Description'                      > $OUTPUT_FILE
+  echo '| ------------- | ------------- | ------------- |' >> $OUTPUT_FILE
+  curl -L --user  "$CREDENTIALS" -s "https://raw.githubusercontent.com/${URI}/master/README.md" | \
+    egrep -E  -o  'https://github.com/.*/.*'      | \
+    tr \" \  | \
+    sed -e 's@[>#"\) ]?@ @g' | \
+    tr '\#' ' ' |        \
+    awk '{print $1}' |   \
+    cut -d\/ -f 4-5  |   \
+    tr -d '\)'       |   \
+    tr -d ':'        |   \
+    sort -du         |   \
+    head -${RESULTS}      |   \
+    while read line ; do \
+      echo "[$line](https://github.com/$line)" \|  \
+      `curl --user  "$CREDENTIALS" -s  -L -k "https://api.github.com/repos/$line" |  \
+      jq -c '[ .stargazers_count  ,"º" ,  .description , "º"] ' | \
+      tr -d '\[' | tr -d '\]' | tr -d ',' | tr -d '\"'  |  tr -d '\|' | tr 'º' '|' `; \
+    done |  sort -r -u -t \| -k2 -n | sed -e 's/^/\|/g' | sed -e 's@) | @) | :star: @g' >> $OUTPUT_FILE
+
+}
+
 AWESOME_LIST_URL=${1:-https://github.com/trimstray/the-book-of-secret-knowledge}
 AWESOME_LIST_URL=https://github.com/josephmisiti/awesome-machine-learning
 AWESOME_LIST_URL=https://github.com/vinta/awesome-python
 RESULTS=5000
-URI=`echo "${AWESOME_LIST_URL}" | egrep -o -e 'github.com/.*' | cut -d\/ -f2-3`
-OUTPUT_FILE=`echo $URI | tr \/ @  | sed -e 's@$@.md@'`
-
-echo '| Link  | Stars   | Description'                      > $OUTPUT_FILE
-echo '| ------------- | ------------- | ------------- |' >> $OUTPUT_FILE
-curl -L --user  "$CREDENTIALS" -s "https://raw.githubusercontent.com/${URI}/master/README.md" | \
-  egrep -E  -o  'https://github.com/.*/.*'      | \
-  tr \" \  | \
-  sed -e 's@[>#"\) ]?@ @g' | \
-  tr '\#' ' ' |        \
-  awk '{print $1}' |   \
-  cut -d\/ -f 4-5  |   \
-  tr -d '\)'       |   \
-  tr -d ':'        |   \
-  sort -du         |   \
-  head -${RESULTS}      |   \
-  while read line ; do \
-    echo "[$line](https://github.com/$line)" \|  \
-    `curl --user  "$CREDENTIALS" -s  -L -k "https://api.github.com/repos/$line" |  \
-    jq -c '[ .stargazers_count  ,"º" ,  .description , "º"] ' | \
-    tr -d '\[' | tr -d '\]' | tr -d ',' | tr -d '\"'  |  tr -d '\|' | tr 'º' '|' `; \
-  done |  sort -r -u -t \| -k2 -n | sed -e 's/^/\|/g' | sed -e 's@) | @) | :star: @g' >> $OUTPUT_FILE
