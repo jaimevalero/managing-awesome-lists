a476e2d24f4bd05d8c82548a9c95ae75fefc9fa1
jaimevalero <jaimevalero78@yahoo.es>
2019-08-15 18:54:22 +0200

    Awesome listin: @gitlost @Greg_Sideyr Well mop it up then https://t.co/jZSTxJ6Stb

--- get_list_details.sh
+++ get_list_details.sh
@@ -16,7 +16,7 @@ Generate_Single_List( )
   URI=`echo "${AWESOME_LIST_URL}" | egrep -o -e 'github.com/.*' | cut -d\/ -f2-3`
   echo Parsing ${URI} ...
 
-  README_NAME=`curl -L --user  "$CREDENTIALS" -s "https:/github.com/${URI}" | grep --colour -o -i /readme.md | head -1 | cut -d\/ -f2  ``
+  README_NAME=`curl -L --user  "$CREDENTIALS" -s "https:/github.com/${URI}" | grep --colour -o -i /readme.md | head -1 | cut -d\/ -f2  `
   # Name of the file
   OUTPUT_FILE=`echo $URI | tr \/ @  | sed -e 's@$@.md@'`
 
