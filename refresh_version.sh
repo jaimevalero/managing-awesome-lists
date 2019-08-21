33ac876a9ab1c6d1e7674d72ff2fee688c2d90e9
jaimevalero <jaimevalero78@yahoo.es>
2019-08-21 21:30:13 +0200

    refresh script

---
+++ refresh_version.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Refresh version,
+# Re-render navbar template
+# download json from api
+# # Re render from the new template
+  ./update-documentation.sh ;
+  ./get_list_details.sh   ;
+
+  git add . ; git commit -m "Refresh version" ;  git push origin master
