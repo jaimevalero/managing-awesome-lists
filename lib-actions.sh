dca3ea696e1445082ca3a476b31bc1f6f699bdeb
jaimevalero <jaimevalero78@yahoo.es>
2019-08-22 18:35:56 +0200

    Templatin'

---
+++ lib-actions.sh
@@ -0,0 +1,24 @@
+LOG_FILE=/var/log/awesome.log
+#######################################################
+#
+# Funcion MostrarLog
+#
+#
+#######################################################
+Log( )
+{
+  echo "[`basename $0`] [`date +'%Y_%m_%d %H:%M:%S'`] [$$] [${FUNCNAME[1]}] $@" >>  $LOG_FILE
+}
+
+source ./.credentials
+
+Download_Api_Repo( )
+{
+  URI="$1"
+  # Name of the file
+  OUTPUT_FILE=`echo $URI | tr \/ @  `
+  #Log Download_Api_Repo $URI $OUTPUT_FILE
+
+  [[  ! -f ".cache/api-${OUTPUT_FILE}.json" ]]  && curl --user  "$CREDENTIALS"  -H "Accept: application/vnd.github.mercy-preview+json"  -s  -L -k "https://api.github.com/repos/$URI" > ./.cache/api-${OUTPUT_FILE}.json
+
+}
--- update-documentation.sh
+++ update-documentation.sh
@@ -2,22 +2,15 @@
 
 # Generate doc for the readme.md file
 
-LOG_FILE=/var/log/awesome.log
-#######################################################
-#
-# Funcion MostrarLog
-#
-#
-#######################################################
-Log( )
-{
-  echo "[`basename $0`] [`date +'%Y_%m_%d %H:%M:%S'`] [$$] [${FUNCNAME[1]}] $@" >>  $LOG_FILE
-}
+
 README_JSON_DATA=/tmp/kk-readme.json
 INDEX_JSON_DATA=/tmp/kk-index.json
+TOPIC_JSON_DATA=/tmp/kk-topic.json
+REPOS_WITH_LABELS_FILE=/tmp/labels.json
 
 README_PARTIAL_DOC=/tmp/kk-readme.md
 INDEX_PARTIAL_DOC=/tmp/kk-index.html
+source ./lib-actions.sh
 
 Generate_Render_Index( )
 {
@@ -52,7 +45,6 @@ Generate_Render_Index( )
 
 }
 
-
 Generate_Readme_Json_Data_Inner( )
 {
   LIST_FILELIST=`find .cache/ | grep readme- | sed -e 's@/readme-@/api-@g' -e 's@.txt$@.json@g'| sort -du `
@@ -67,14 +59,17 @@ Generate_Readme_Json_Data_Inner( )
   echo " } "
 
 }
+
 Generate_Index( )
 {
   ./generate_render_template.py  -t template-index.html --data ${INDEX_JSON_DATA}
 }
+
 Generate_Partial_Doc( )
 {
   ./generate_render_template.py  -t template-readme.html --data ${README_JSON_DATA}
 }
+
 Generate_Readme_Json_Data( )
 {
   Generate_Readme_Json_Data_Inner| tr '\n' ' ' | sed -e  's@, ]@]@g'
@@ -102,6 +97,61 @@ Document_Navbar( )
   ls -altr  views/layout/navbar.html
 }
 
+Document_Topics( )
+{
+  cat $INDEX_JSON_DATA | jq -c ".repos[]|{ full_name , topics}" | grep -v '"topics":\[\]'  > ${REPOS_WITH_LABELS_FILE}
+
+  # Label in more than one repo
+  TOPICS_LIST=` cat $INDEX_JSON_DATA | jq ".repos[]|.topics"   | grep \" | cut -d\" -f2 | sort  | uniq -c | sort -k1 -n  -r | grep -v "   1 "  |  awk '{print $2}'  | head`
+
+  rm -rf var/topics/     ; mkdir var/topics/
+  rm -rf .cache/topics/  ; mkdir .cache/topics/
+
+  for topic in $TOPICS_LIST
+  do
+    Log "Topic: $topic"
+    TOPIC_FILE=".cache/topics/$topic"
+    grep "\"$topic\"" ${REPOS_WITH_LABELS_FILE} | jq -r .full_name >> "${TOPIC_FILE}"
+    Generate_Topic_Json "${TOPIC_FILE}" > $TOPIC_JSON_DATA
+    Generate_Render_Topic $TOPIC_JSON_DATA
+  done
+
+
+}
+
+
+Generate_Topic_Json( )
+{
+  TOPIC_FILE="$1"
+  TOPIC_NAME=`basename "$1"`
+  Log TOPIC_NAME=$TOPIC_NAME= TOPIC_FILE=$TOPIC_FILE=
+  # Check every repo is in cache
+  while read my_topic_file ; do Download_Api_Repo "$my_topic_file" ; done < "${TOPIC_FILE}"
+
+  echo " { \"list\" : \"${TOPIC_NAME}\"  , \"repos\" :  "
+  cat "${TOPIC_FILE}" | tr \/ \@ | sed -e 's@^@.cache/api-@g' -e 's@$@.json@g' | xargs cat | jq -c  "
+   {
+     full_name,
+     description,
+     topics,
+     created_at,
+     pushed_at,
+     stargazers_count,
+     language
+      }"  | jq  --slurp .
+  echo " } "
+
+}
+
+
+
+Generate_Render_Topic( )
+{
+  TOPIC_JSON_DATA="$1"
+  TOPIC=`cat $TOPIC_JSON_DATA | jq -r .list  | cut -d\/ -f3-100`
+  ./generate_render_template.py  -t template-topic.html --data ${TOPIC_JSON_DATA} > "var/topics/$TOPIC.html"
+}
+
 Main( )
 {
 
@@ -116,28 +166,14 @@ Main( )
   # Document navbar
   Document_Navbar
   Generate_Render_Index
+
+  # Topics
+  Document_Topics
 }
 
-Main
+
+
+source ./lib-actions.sh
+
+#Main
 #git add . ; git commit -m "Templatin'" ;  git push origin master
-REPOS_WITH_LABELS_FILE=/tmp/labels.json
-
-cat $INDEX_JSON_DATA | jq -c ".repos[]|{ full_name , topics}" | grep -v '"topics":\[\]'  > ${REPOS_WITH_LABELS_FILE}
-
-# Label in more than one repo
-TOPICS_LIST=` cat $INDEX_JSON_DATA | jq ".repos[]|.topics"   | grep \" | cut -d\" -f2 | sort  | uniq -c | sort -k1 -n  -r | grep -v "   1 "  | head | awk '{print $2}'`
-
-
-rm -rf .cache/topics/  ; mkdir .cache/topics/
-
-for topic in $TOPICS_LIST
-do
-  grep "\"$topic\"" ${REPOS_WITH_LABELS_FILE} | jq -r .full_name >> ".cache/topics/$topic"
-
-done
-
-
-
-
-
-#for i in
