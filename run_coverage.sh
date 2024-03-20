d1e07716db638aab54e8ec43ac6a9409ca6565fd
jaimevalero <jaime.valeromarquez@telefonica.com>
2024-03-20 14:49:54 +0100

    Add github action

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Hay que crear aqui un virtual env con python 3.10 y activarlo
+conda activate ` basename $PWD`
 
 python -m coverage run -m unittest discover -p test_*.py -s tests
 python -m coverage report 
